0dbc9655706b5348bf2a5c95eed07c514987b265
armaan <adhanji8@gmail.com>
2023-02-06 12:23:50 -0800

    fix checkAuth typeerrors

--- middleware/checkAuth.ts
+++ middleware/checkAuth.ts
@@ -1,7 +1,8 @@
+import { Request, Response } from "express";
 /*
 FIX ME (types) 😭
 */
-export const ensureAuthenticated = (req: any, res: any, next: any) => {
+export const ensureAuthenticated = (req: Request, res: Response, next: any) => {
   if (req.isAuthenticated()) {
     return next();
   }
